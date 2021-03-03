10c9c77a153d6804106f0096fde1146f4dbc3cfe
Michal Podrouzek <michallo64@gmail.com>
2021-03-03 19:28:10 +0100

    Game complete

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
         ZStack{
             Image("background").edgesIgnoringSafeArea(.all)
@@ -18,14 +23,28 @@ struct ContentView: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Image("card3")
+                    Image(playerCard)
                     Spacer()
-                    Image("card4")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
                 Button(action: {
-                    print("Deal")
+                    
+                    // Generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update the cards
+                    playerCard = "card"+String(playerRand)
+                    cpuCard = "card"+String(cpuRand)
+                    // Update the score
+                    if(playerRand > cpuRand){
+                        playerScore += 1
+                    }else if(cpuRand > playerRand){
+                        cpuScore += 1
+                    }
+                    
                 }, label: {
                     Image("dealbutton")
                 })
@@ -34,12 +53,12 @@ struct ContentView: View {
                     Spacer()
                     VStack{
                         Text("Player").font(.headline).foregroundColor(Color.white).padding(.bottom, 10.0)
-                        Text("0").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(Color.white)
+                        Text(String(playerScore)).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(Color.white)
                     }
                     Spacer()
                     VStack{
                         Text("Player").font(.headline).foregroundColor(Color.white).padding(.bottom, 10.0)
-                        Text("0").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(Color.white)
+                        Text(String(cpuScore)).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(Color.white)
                     }
                     Spacer()
                 }
